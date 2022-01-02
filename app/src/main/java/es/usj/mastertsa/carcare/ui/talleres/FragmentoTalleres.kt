a3519ba8564293c72e16b3d48e8dd7cef117d264
ghtdeveloper <martinezfriasedison@gmail.com>
2022-01-02 18:35:17 -0400

    Se agrego el layout en  el modulo de taller

--- app/src/main/java/es/usj/mastertsa/carcare/ui/talleres/FragmentoTalleres.kt
+++ app/src/main/java/es/usj/mastertsa/carcare/ui/talleres/FragmentoTalleres.kt
@@ -1,11 +1,14 @@
 package es.usj.mastertsa.carcare.ui.talleres
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import es.usj.mastertsa.carcare.R
+import es.usj.mastertsa.carcare.databinding.FragmentoTalleresBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +21,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FragmentoTalleres : Fragment() {
+
+    private lateinit var binding: FragmentoTalleresBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,14 +34,20 @@ class FragmentoTalleres : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        //Listener
+
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
+        Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmento_talleres, container, false)
+        binding = FragmentoTalleresBinding.inflate(layoutInflater)
+        binding.floatingActionButton.setOnClickListener {
+           mostrarRegistrarTaller()
+        }
+        return binding.root
     }
 
     companion object {
@@ -57,4 +69,21 @@ class FragmentoTalleres : Fragment() {
                 }
             }
     }
-}
+
+    private fun mostrarRegistrarTaller()
+    {
+        val dialoBuilder = AlertDialog.Builder(requireContext())
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_registar_taller,null)
+        dialoBuilder.setView(dialogView)
+        //Buttons
+        dialoBuilder.setPositiveButton(R.string.text_registrar) { _, i ->
+            Toast.makeText(requireContext(), "Soy el boton aceptar", Toast.LENGTH_LONG).show()
+        }//Aceptar
+        dialoBuilder.setNegativeButton(R.string.text_cancelar) { _, i -> }//Cancelar
+        val alertDialog = dialoBuilder.create()
+        alertDialog.show()
+
+    }//Fin del metodo mostrarRegistrarTaller
+
+}//Fin de la class FragmentoTalleres
