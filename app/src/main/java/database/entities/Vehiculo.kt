80f21194531b67f30d3d867b8fc73c4d7786d3e7
Miguel Jimenez <chamuel.developer@gmail.com>
2022-01-05 19:32:24 -0400

    WIP: Integrando ROOM TODO: Crear clase App para conectar(Segun el tuto) XD.

---
+++ app/src/main/java/database/entities/Vehiculo.kt
@@ -0,0 +1,17 @@
+package database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "vehiculo")
+data class Vehiculo (
+@PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val marca: String,
+    val modelo: String,
+    val anioFabricacion: String,
+    val colorVehiculor: String,
+    val VIN: String,
+    val Alias: String
+
+    )
